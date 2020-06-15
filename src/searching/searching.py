162e6f1ea8e257df4c97f011fc6c525bd5753157
Brandon-Harris <brandonharris177@gmail.com>
2020-06-15 15:32:26 -0600

    linear_search complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,15 @@
 def linear_search(arr, target):
     # Your code here
-
+    count = 0
+    for i in arr:
+        if i == target:
+            # print(count)
+            return count
+        count = count+1
 
     return -1   # not found
 
+linear_search([3, 2, 1, 9, 7, 4, 5], 7)
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
